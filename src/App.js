156ff828d297e240fb142386aeee2db48a90b306
Zsolt Nagy <info@zsoltnagy.eu>
2024-06-01 18:08:24 +0200

    parking lot form and list empty components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import "./App.css";
 
+import ParkingLotForm from "./Components/ParkingLotForm/ParkingLotForm";
+import ParkingLotList from "./Components/ParkingLotList/ParkingLotList";
+
 export default function App() {
     return (
         <div className="App">
@@ -7,6 +10,10 @@ export default function App() {
                 <h1>Browser Parking Lot</h1>
                 <p>Send most of your browser tabs into retirement.</p>
             </header>
+            <main>
+                <ParkingLotForm />
+                <ParkingLotList />
+            </main>
         </div>
     );
 }
---
+++ src/Components/ParkingLotList/ParkingLotList.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function ParkingLotList() {
+    return (
+        <div>ParkingLotList</div>
+    )
+}
