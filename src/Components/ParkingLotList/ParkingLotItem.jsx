a1a318fd6e02b599a222fa7a8093e0da75788e91
Zsolt Nagy <info@zsoltnagy.eu>
2024-06-06 02:37:29 +0200

    react card update

--- src/Components/ParkingLotList/ParkingLotItem.jsx
+++ src/Components/ParkingLotList/ParkingLotItem.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
+import { Card, CardHeader, CardBody, CardTitle, CardText } from 'reactstrap';
 
 export default function ParkingLotItem({ id, date, priority, link, description}) {
     return (
-        <section className="parking-lot-item-container">
-            <p className="parking-lot-item-field">Date: { date }</p>
-            <p className="parking-lot-item-field">Priority: { priority }</p>
-            <p className="parking-lot-item-field">Description: { description}</p>
-            <p className="parking-lot-item-field">
-                Link:
-                <a href={link} target="_blank" className="parking-lot-item-anchor">
-                    {link}
-                </a>
-            </p>
-        </section>
+        <Card 
+            className="my-2 parking-lot-item-container" 
+            color="secondary"
+            inverse>
+            <CardHeader>
+                { date } (Priority: {priority})
+            </CardHeader>
+            <CardBody>
+                <CardTitle tag="h5">
+                    { description }
+                </CardTitle>
+                <CardText>
+                    Link:&nbsp;
+                    <a href={link} target="_blank" className="parking-lot-item-anchor">
+                        {link}
+                    </a>
+                </CardText>
+            </CardBody>
+        </Card>
     )
 }
